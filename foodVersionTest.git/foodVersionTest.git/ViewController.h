630764f266fab76e75181ca7e7d61c9da548fce6
Dementiev Nikolay <dementievnikolay@gmail.com>
2017-11-14 16:13:07 +0200

    initial

---
+++ foodVersionTest.git/foodVersionTest.git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  foodVersionTest.git
+//
+//  Created by Nikolay Dementiev on 14.11.17.
+//  Copyright © 2017 eda.ua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ foodVersionTest.git/foodVersionTest.git/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  foodVersionTest.git
+//
+//  Created by Nikolay Dementiev on 14.11.17.
+//  Copyright © 2017 eda.ua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
